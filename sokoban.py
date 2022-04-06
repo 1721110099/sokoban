67d0ff89f3d804c53f42a8c049cb3ab2ddad1621
1721110099 <97765494+1721110099@users.noreply.github.com>
2022-04-06 02:58:55 +0000

    Pruebas de movDer con print inicio

--- sokoban.py
+++ sokoban.py
@@ -32,8 +32,8 @@ Reglas validas para moverse (Arriba, Derecha, Abajo, Izquierda)
   """
   
   mapa = [
-      [1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [1,5,3,0,0,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0],
+      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [1,5,0,4,0,3,3,3,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0],
       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1],
       [1,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1],
       [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1],
@@ -69,69 +69,80 @@ Reglas validas para moverse (Arriba, Derecha, Abajo, Izquierda)
     """
     #00 - Personaje, espacio -> [5,0] -> [0,5]
     if self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 0:
-        print ("Personaje, espacio")
+        print ("MovDer: Personaje, espacio")
         self.mapa[self.personaje_fila][self.personaje_columna] = 0
         self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
         self.personaje_columna = self.personaje_columna + 1
     #01 -Personaje, meta -> [5,3] -> [0,8]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 3:
-      self.mapa[self.personaje_fila][self.personaje_columna] = 0
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 8
-      self.personaje_columna = self.personaje_columna + 1
+        print("MovDer:Personaje, meta")
+        self.mapa[self.personaje_fila][self.personaje_columna] = 0
+        self.mapa[self.personaje_fila][self.personaje_columna + 1] = 8
+        self.personaje_columna = self.personaje_columna + 1
     #02 - Personaje, caja, espacio -> [5,4,0] -> [0,5,4]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 4 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 0:
+      print("MovDer: Personaje, caja, espacio ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 0
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 5
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 4
       self.personaje_columna = self.personaje_columna + 1
     #03 - Personaje, caja, meta -> [5,4,3] -> [0,5,7]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 4 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 3:
+      print("MovDer:Personaje, caja, meta ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 0
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 5
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 7
       self.personaje_columna = self.personaje_columna + 1
     #04 - Personaje, caja_meta, espacio -> [5,7,0] -> [0,8,4]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 7 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 0:
+      print("MovDer: Personaje, caja_meta, espacio")
       self.mapa[self.personaje_fila][self.personaje_columna] = 0
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 8
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 4
       self.personaje_columna = self.personaje_columna + 1
     #05 - Personaje, caja_meta, meta -> [5,7,3] -> [0,8,7]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 7 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 3:
+      print("MovDer: Personaje, caja_meta, meta ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 0
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 8
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 7
       self.personaje_columna = self.personaje_columna + 1
     #06 -Personaje_meta, espacio -> [8,0] -> [3,5]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 0:
+      print("MovDer:Personaje_meta, espacio ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
       self.personaje_columna = self.personaje_columna + 1
     #07 -Personaje_meta, meta -> [8,3] -> [3,8]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 3:
+      print("MovDer:Personaje_meta, meta  ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 8
       self.personaje_columna = self.personaje_columna + 1
     #08 - Personaje_meta, caja, espacio -> [8,4,0] -> [3,5,4]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 4 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 0:
+      print("MovDer:Personaje_meta, caja, espacio ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 5
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 4
       self.personaje_columna = self.personaje_columna + 1
     #09 - Personaje_meta, caja, meta -> [8,4,3] -> [3,8,7]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 4 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 3:
+      print("MovDer: Personaje_meta, caja, meta")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 8
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 7
       self.personaje_columna = self.personaje_columna + 1
     #10 - Personaje_meta, caja_meta, espacio -> [8,7,0] -> [3,8,4]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 7 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 0:
+      print("MovDer: Personaje_meta, caja_meta, espacio")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 8
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 4
       self.personaje_columna = self.personaje_columna + 1
-    #11 - Personaje_meta, caja_meta, espacio -> [8,7,3] -> [3,8,7]
+    #11 - Personaje_meta, caja_meta, meta -> [8,7,3] -> [3,8,7]
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 8 and self.mapa[self.personaje_fila][self.personaje_columna +1] == 7 and self.mapa[self.personaje_fila][self.personaje_columna +2] == 3:
+      print("MovDer: Personaje_meta, caja_meta, meta ")
       self.mapa[self.personaje_fila][self.personaje_columna] = 3
       self.mapa[self.personaje_fila][self.personaje_columna +1] = 8
       self.mapa[self.personaje_fila][self.personaje_columna +2] = 7
